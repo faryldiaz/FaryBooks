44d7eb31b1c7fe644f2dc1c57c05220a1c45948c
faryldiaz <darcy.swag.on@gmail.com>
2026-09-23 17:42:46 -0500

    Initialize build.gradle.kts for Android app

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.farybooks.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.farybooks.app"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 2
+        versionName = "0.2"
+    }
+}
